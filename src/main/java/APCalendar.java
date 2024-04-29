756fb1fce93057f76ab38c95d2a611aaa67a6895
Sebastian Dowell <142616573+SebastianAPCS@users.noreply.github.com>
2024-04-29 00:28:44 +0000

    write class

--- src/main/java/APCalendar.java
+++ src/main/java/APCalendar.java
@@ -10,10 +10,12 @@ public class APCalendar
   /** Returns the number of leap years between year1 and year2, inclusive.
    * Precondition: 0 <= year1 <= year2
    */
-  public static int numberOfLeapYears(int year1, int year2)
-  { 
-    /* to be implemented in part (a) */
-
+  public static int numberOfLeapYears(int year1, int year2) { 
+    int count = 0;
+    for (int i = year1; i <= year2; i++) {
+      if (isLeapYear(i)) count++;
+    }
+    return count;
   }
   
   /** Returns the value representing the day of the week for the first day of year,
@@ -49,8 +51,7 @@ public class APCalendar
    *  and 6 denotes Saturday.
    *  Precondition: The date represented by month, day, year is a valid date.
    */
-  public static int dayOfWeek(int month, int day, int year)
-  {
-    /* to be implemented in part (b) */
+  public static int dayOfWeek(int month, int day, int year) {
+    return (firstDayOfYear(year) + dayOfYear(month, day, year) - 1) % 7;
   }
 }
